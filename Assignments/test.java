ef46b6c0b9c4faaf3cbc184f066149fdff7f5575
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-10-12 22:22:26 -0400

    asa

--- Assignments/test.java
+++
@@ -1,7 +0,0 @@
-int[][] hitPoints = { {4, 6, 8, 10}, {7, 10, 13, 16}, {10, 14, 18, 22} };
-for (int level = 0; level < hitPoints.length; ++level) {
-    for (int class = 0; class < hitPoints[level].length; ++class) {
-        System.out.print(hitPoints[level][class]);
-        System.out.print("\t");
-    }
-    System.out.printf("%n"); // Could also use \n.
