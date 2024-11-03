333333d2362a9290928aa9b63606f74a42a01833
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-11-02 20:09:31 -0400

    menu

--- sodoku/SudokuPuzzle.java
+++ sodoku/SudokuPuzzle.java
@@ -32,3 +32,20 @@ public class SudokuPuzzle {
             // Print the puzzle
             System.out.println("Here's your newly generated Sudoku puzzle:");
             printSudoku(puzzle);
+            // Menu options
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Show solution");
+            System.out.println("2. Generate new puzzle");
+            System.out.println("3. Quit");
+            
+            choice = scanner.nextLine();
+            
+            if (choice.equals("1")) {
+                System.out.println("\nHere's the solution:");
+                printSudoku(fullGrid);
+            } else if (choice.equals("2")) {
+                System.out.println("\nGenerating a new puzzle...");
+            } else if (!choice.equals("3")) {
+                System.out.println("\nInvalid choice. Please try again.");
+            }
+        }
