9cd3b2cdbcf3680dbdffb4d8779154eb1cba1211
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-11-02 20:07:50 -0400

    random

--- sodoku/SudokuPuzzle.java
+++ sodoku/SudokuPuzzle.java
@@ -7,11 +7,12 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SudokuPuzzle {
-    import java.util.Random;
-    import java.util.Scanner;
-    
-    public class SudokuPuzzle {
-        // creates Sudoku grid size
-        private static final int GRID_SIZE = 9;
-        private static final int BOX_SIZE = 3;
- 
+    // creates Sudoku grid size
+    private static final int GRID_SIZE = 9;
+    private static final int BOX_SIZE = 3;
+
+    // import Random numbers for puzzle
+    private static Random random = new Random();
+
+    //  User input
+    private static Scanner scanner = new Scanner(System.in);
